9a9296cbf34a7d1fd2da63a0cd3ed99b8be166ad
Will Miller <me@willrmiller.com>
2023-11-11 06:55:21 -0500

    Added team projects

---
+++ GameJam_TeamD/GameJam10_2023/scripts/bullet.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var direction = Vector2.ZERO
+var shooter = null
+var SPEED = 500
+
+func _process(delta):
+	#move the bullet
+	position += direction * SPEED * delta
+
+func _on_body_entered(body):
+	#hit things
+	if body != shooter:
+		queue_free()
+		
+		#deal damage to the thing hit
+		if(body.has_method("take_damage")):
+			body.take_damage(1)
