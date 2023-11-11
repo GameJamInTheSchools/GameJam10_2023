e59a5eb32eb2916a220495554142595ce2b85829
Will Miller <me@willrmiller.com>
2023-11-11 06:53:32 -0500

    Fixing issue with setup script

--- Utils/Setup.bat
+++ Utils/Setup.bat
@@ -13,13 +13,6 @@ git init
 echo Adding remote origin...
 git remote add origin https://github.com/GameJamInTheSchools/GameJam10_2023.git
 
-echo Checking if Team %TEAM_NAME% directory exists in the repository...
-git ls-remote --exit-code --heads origin "refs/heads/GameJam_%TEAM_NAME%" >nul 2>nul
-if errorlevel 1 (
-    echo Team %TEAM_NAME% does not exist in the repository. Please provide a valid team name.
-    exit /b 1
-)
-
 echo Setting up sparse-checkout for team $TEAM_NAME%...
 git sparse-checkout set GameJam_%TEAM_NAME%
 
