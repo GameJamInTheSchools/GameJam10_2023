2de6d7b1ea5f92b21937d4a71332aefcc391e224
AvivaKitty <66836632+GameJamInTheSchools@users.noreply.github.com>
2023-11-11 15:46:48 -0500

    4d
    level select

--- GameJam_TeamD/GameJam10_2023/scripts/screens/level_select.gd
+++ GameJam_TeamD/GameJam10_2023/scripts/screens/level_select.gd
@@ -19,7 +19,7 @@ func _on_scene_4_pressed():
 
 
 func _on_scene_5_pressed():
-	get_tree().change_scene_to_file("res://node_2djf.tscn")
+	get_tree().change_scene_to_file("res://boss_battle.tscn")
 
 
 func _on_testing_pressed():
